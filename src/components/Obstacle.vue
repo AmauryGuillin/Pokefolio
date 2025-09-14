0d13c7e57abff95f8796054f185b912fe80633e1
AmauryGuillin <amauryg@hotmail.fr>
2025-09-14 18:17:28 +0200

    refactor: obstacles

---
+++ src/components/Obstacle.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  showObstacles: boolean
+  cellWidth: number
+  cellHeight: number
+}>()
+</script>
+
+<template>
+  <div
+    class="absolute bg-contain bg-no-repeat bg-center cursor-default"
+    :style="{
+      backgroundColor: `${showObstacles ? 'red' : ''}`,
+      width: `${cellWidth}px`,
+      height: `${cellHeight}px`,
+    }"
+  >
+    <!-- {{ cell }} -->
+  </div>
+</template>
