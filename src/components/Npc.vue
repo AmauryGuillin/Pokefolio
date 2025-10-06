827152c58b025a5c81138b5aa396299a47388446
AmauryGuillin <amauryg@hotmail.fr>
2025-10-06 19:59:20 +0200

    feat: add npc name when hover

--- src/components/Npc.vue
+++ src/components/Npc.vue
@@ -1,23 +1,35 @@
 <script setup lang="ts">
 import type { NPC } from '@/utils/npc'
 import { getImage } from '@/utils/utils'
+import { ref } from 'vue'
 
-defineProps<{
+const props = defineProps<{
   npc: NPC
   cell: number
   cellWidth: number
   cellHeight: number
 }>()
+
+const hover = ref(false)
 </script>
 
 <template>
   <div
     v-if="npc.position === cell"
-    class="absolute bg-contain bg-no-repeat bg-center cursor-pointer"
+    @mouseenter="hover = !hover"
+    @mouseleave="hover = !hover"
+    class="bg-contain bg-no-repeat bg-center cursor-pointer relative"
     :style="{
       backgroundImage: `url(${getImage('npcs', npc.image || '')})`,
       width: `${cellWidth}px`,
       height: `${cellHeight}px`,
     }"
-  ></div>
+  >
+    <div
+      v-if="hover"
+      class="absolute -top-7 bg-black/70 font-bold p-2 whitespace-nowrap overflow-visible z-10"
+    >
+      {{ npc.name.french }}
+    </div>
+  </div>
 </template>
