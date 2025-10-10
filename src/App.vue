ac23edca153962be6fea2b1fd8939cb3f0c1a870
AmauryGuillin <amauryg@hotmail.fr>
2025-10-10 23:40:42 +0200

    fix: make app unavailable for ultrawide screens

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,27 @@ import SmallScreens from './components/SmallScreens.vue'
 
 const { width, height } = useWindowSize()
 const isScreenLargeEnough = ref<boolean>(true)
+const isScreenTooLarge = ref<boolean>(false)
 
 const minWidth = 1080
 const minHeight = 650
+const maxWidth = 3840
+const maxHeight = 2160
 
 watch(
   [width, height],
   () => {
     isScreenLargeEnough.value = width.value >= minWidth && height.value >= minHeight
+    isScreenTooLarge.value = width.value > maxWidth || height.value > maxHeight
+    console.log('isScreenLargeEnough', isScreenLargeEnough.value)
+    console.log('isScreenTooLarge', isScreenTooLarge.value)
   },
   { immediate: true },
 )
 </script>
 
 <template>
-  <div v-if="isScreenLargeEnough">
+  <div v-if="isScreenLargeEnough && !isScreenTooLarge">
     <Toaster position="top-center" richColors />
     <RouterView />
   </div>
--- src/components/SmallScreens.vue
+++ src/components/SmallScreens.vue
@@ -7,10 +7,12 @@
     <img src="/unavailable-icon.png" alt="pokemon-icon" class="h-[20%]" />
     <p class="bg-black/75 text-white font-bold p-2 text-xs">Welcome to my portfolio!</p>
     <p class="bg-black/75 text-white font-bold p-2 text-xs">
-      Unfortunately, this website isn't available on small devices like smartphones.
+      Unfortunately, this website isn't available on small devices like smartphones or ultrawide
+      screens.
     </p>
     <p class="bg-black/75 text-white font-bold p-2 text-xs">
-      For the best experience, please use a larger screen.
+      For the best experience and depending on your current device, please use a larger or smaller
+      screen.
     </p>
     <p class="bg-black/75 text-white font-bold p-2 text-xs">
       However, you can still access my public projects here: <br />
