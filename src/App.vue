9a01800174460e16e30469f370275123c21eda1e
AmauryGuillin <amauryg@hotmail.fr>
2025-10-10 12:39:59 +0200

    feat: limit screen size

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,32 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { Toaster } from '@/components/ui/sonner'
 import 'vue-sonner/style.css'
+import { useWindowSize } from '@vueuse/core'
+import { ref, watch } from 'vue'
+
+const { width, height } = useWindowSize()
+const isScreenLargeEnough = ref<boolean>(true)
+
+const minWidth = 900
+const minHeight = 600
+
+watch(
+  [width, height],
+  () => {
+    isScreenLargeEnough.value = width.value >= minWidth && height.value >= minHeight
+  },
+  { immediate: true },
+)
 </script>
 
 <template>
-  <header>
-    <!-- <nav>
-      <RouterLink to="/">Landing</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav> -->
-  </header>
-  <Toaster position="top-center" richColors />
-  <RouterView />
+  <div v-if="isScreenLargeEnough">
+    <Toaster position="top-center" richColors />
+    <RouterView />
+  </div>
+  <div v-else class="flex justify-center items-center h-screen bg-gray-800">
+    <p class="bg-black/75 text-white font-bold p-2">
+      For an optimal experience, please use a bigger screen.
+    </p>
+  </div>
 </template>
