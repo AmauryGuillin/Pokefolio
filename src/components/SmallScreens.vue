0cf3b46cd39dc0c4b7acca56105256bffdaa4a78
AmauryGuillin <amauryg@hotmail.fr>
2025-10-10 19:43:07 +0200

    style: better spelling for unavailable screen

--- src/components/SmallScreens.vue
+++ src/components/SmallScreens.vue
@@ -5,15 +5,15 @@
     class="flex flex-col gap-3 justify-center items-center h-screen bg-gray-800 font-(family-name:--font-game)"
   >
     <img src="/unavailable-icon.png" alt="pokemon-icon" class="h-[20%]" />
-    <p class="bg-black/75 text-white font-bold p-2 text-xs">Welcome to my portfolio !</p>
+    <p class="bg-black/75 text-white font-bold p-2 text-xs">Welcome to my portfolio!</p>
     <p class="bg-black/75 text-white font-bold p-2 text-xs">
       Unfortunately, this website isn't available on small devices like smartphones.
     </p>
     <p class="bg-black/75 text-white font-bold p-2 text-xs">
-      For an optimal experience, please use a bigger screen.
+      For the best experience, please use a larger screen.
     </p>
     <p class="bg-black/75 text-white font-bold p-2 text-xs">
-      Nonetheless, you can access to my public projects here: <br />
+      However, you can still access my public projects here: <br />
       <br />
       >
       <a
